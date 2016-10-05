3ba63d127aa696516f982fb5771a8331d459bcf1
fobnn <fobnn2012@gmail.com>
2016-10-05 17:54:59 +0800

    n^2 solution,try to find out faster way

---
+++ 239. Sliding Window Maximum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> vec;
+        int nmax = nums[0];
+
+        for(int i = 1; i < k ; ++i )
+        {
+            if( nmax < nums[i])
+            {
+                nmax = nums[i];
+            }
+        }
+        vec.push_back(nmax);
+
+        for(int i = k ; i < nums.size() && i - k >=0; ++i )
+        {
+            for(int j = i;j> i-k ; --j)
+            {
+                if( nmax < nums[j])
+                {
+                    nmax = nums[j];
+                }
+            }
+            vec.push_back(nmax);
+        }
+        return vec;
+    }
+};
